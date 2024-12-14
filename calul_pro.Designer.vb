3657704a2f0ea8990a19b8fb5a795ab661d5ad57
zeine <zeine@ZEINEB_PC>
2024-12-14 20:45:45 +0100

    Ajoutez des fichiers projet.

---
+++ calul_pro.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class calul_pro
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox = New TextBox()
+        Button_0 = New Button()
+        Button_1 = New Button()
+        Button_2 = New Button()
+        button_3 = New Button()
+        Button_4 = New Button()
+        Button_5 = New Button()
+        button_6 = New Button()
+        Button_7 = New Button()
+        Button_8 = New Button()
+        Button_c = New Button()
+        Button_plus = New Button()
+        Button_moins = New Button()
+        Button_div = New Button()
+        Button_fois = New Button()
+        Button_egal = New Button()
+        Button_9 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox
+        ' 
+        TextBox.Location = New Point(160, 36)
+        TextBox.Name = "TextBox"
+        TextBox.Size = New Size(358, 27)
+        TextBox.TabIndex = 0
+        ' 
+        ' Button_0
+        ' 
+        Button_0.BackColor = SystemColors.ActiveCaption
+        Button_0.FlatStyle = FlatStyle.Popup
+        Button_0.Font = New Font("Segoe UI Variable Display Semib", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button_0.ForeColor = SystemColors.ActiveCaptionText
+        Button_0.Location = New Point(12, 163)
+        Button_0.Name = "Button_0"
+        Button_0.Size = New Size(94, 29)
+        Button_0.TabIndex = 1
+        Button_0.Text = "0"
+        Button_0.UseVisualStyleBackColor = False
+        ' 
+        ' Button_1
+        ' 
+        Button_1.BackColor = SystemColors.ActiveCaption
+        Button_1.FlatStyle = FlatStyle.Popup
+        Button_1.Font = New Font("Segoe UI Symbol", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_1.ForeColor = SystemColors.ActiveCaptionText
+        Button_1.Location = New Point(126, 163)
+        Button_1.Name = "Button_1"
+        Button_1.Size = New Size(94, 29)
+        Button_1.TabIndex = 2
+        Button_1.Text = "1"
+        Button_1.UseVisualStyleBackColor = False
+        ' 
+        ' Button_2
+        ' 
+        Button_2.BackColor = SystemColors.ActiveCaption
+        Button_2.FlatStyle = FlatStyle.Popup
+        Button_2.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_2.Location = New Point(246, 163)
+        Button_2.Name = "Button_2"
+        Button_2.Size = New Size(94, 29)
+        Button_2.TabIndex = 3
+        Button_2.Text = "2"
+        Button_2.UseVisualStyleBackColor = False
+        ' 
+        ' button_3
+        ' 
+        button_3.BackColor = SystemColors.ActiveCaption
+        button_3.FlatStyle = FlatStyle.Popup
+        button_3.Font = New Font("Segoe UI Symbol", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        button_3.ForeColor = SystemColors.ActiveCaptionText
+        button_3.Location = New Point(12, 210)
+        button_3.Name = "button_3"
+        button_3.Size = New Size(94, 29)
+        button_3.TabIndex = 4
+        button_3.Text = "3"
+        button_3.UseVisualStyleBackColor = False
+        ' 
+        ' Button_4
+        ' 
+        Button_4.BackColor = SystemColors.ActiveCaption
+        Button_4.FlatStyle = FlatStyle.Popup
+        Button_4.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_4.Location = New Point(126, 210)
+        Button_4.Name = "Button_4"
+        Button_4.Size = New Size(94, 29)
+        Button_4.TabIndex = 5
+        Button_4.Text = "4"
+        Button_4.UseVisualStyleBackColor = False
+        ' 
+        ' Button_5
+        ' 
+        Button_5.BackColor = SystemColors.ActiveCaption
+        Button_5.FlatStyle = FlatStyle.Popup
+        Button_5.Font = New Font("Segoe UI Emoji", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_5.Location = New Point(246, 210)
+        Button_5.Name = "Button_5"
+        Button_5.Size = New Size(94, 29)
+        Button_5.TabIndex = 6
+        Button_5.Text = "5"
+        Button_5.UseVisualStyleBackColor = False
+        ' 
+        ' button_6
+        ' 
+        button_6.BackColor = SystemColors.ActiveCaption
+        button_6.FlatStyle = FlatStyle.Popup
+        button_6.Font = New Font("Segoe UI Symbol", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        button_6.Location = New Point(12, 273)
+        button_6.Name = "button_6"
+        button_6.Size = New Size(94, 29)
+        button_6.TabIndex = 7
+        button_6.Text = "6"
+        button_6.UseVisualStyleBackColor = False
+        ' 
+        ' Button_7
+        ' 
+        Button_7.BackColor = SystemColors.ActiveCaption
+        Button_7.FlatStyle = FlatStyle.Popup
+        Button_7.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button_7.Location = New Point(126, 273)
+        Button_7.Name = "Button_7"
+        Button_7.Size = New Size(94, 29)
+        Button_7.TabIndex = 8
+        Button_7.Text = "7"
+        Button_7.UseVisualStyleBackColor = False
+        ' 
+        ' Button_8
+        ' 
+        Button_8.BackColor = SystemColors.ActiveCaption
+        Button_8.FlatStyle = FlatStyle.Popup
+        Button_8.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_8.Location = New Point(246, 273)
+        Button_8.Name = "Button_8"
+        Button_8.Size = New Size(94, 29)
+        Button_8.TabIndex = 9
+        Button_8.Text = "8"
+        Button_8.UseVisualStyleBackColor = False
+        ' 
+        ' Button_c
+        ' 
+        Button_c.BackColor = SystemColors.ControlDarkDark
+        Button_c.FlatStyle = FlatStyle.Popup
+        Button_c.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button_c.ForeColor = SystemColors.Control
+        Button_c.Location = New Point(82, 383)
+        Button_c.Name = "Button_c"
+        Button_c.Size = New Size(589, 55)
+        Button_c.TabIndex = 10
+        Button_c.Text = "c"
+        Button_c.UseVisualStyleBackColor = False
+        ' 
+        ' Button_plus
+        ' 
+        Button_plus.BackColor = SystemColors.ActiveCaptionText
+        Button_plus.FlatStyle = FlatStyle.Popup
+        Button_plus.Font = New Font("Forte", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_plus.ForeColor = SystemColors.ButtonFace
+        Button_plus.Location = New Point(428, 163)
+        Button_plus.Name = "Button_plus"
+        Button_plus.Size = New Size(149, 50)
+        Button_plus.TabIndex = 11
+        Button_plus.Text = "+"
+        Button_plus.UseVisualStyleBackColor = False
+        ' 
+        ' Button_moins
+        ' 
+        Button_moins.BackColor = SystemColors.ActiveCaptionText
+        Button_moins.FlatStyle = FlatStyle.Popup
+        Button_moins.Font = New Font("Showcard Gothic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_moins.ForeColor = SystemColors.ButtonFace
+        Button_moins.Location = New Point(602, 163)
+        Button_moins.Name = "Button_moins"
+        Button_moins.Size = New Size(147, 49)
+        Button_moins.TabIndex = 12
+        Button_moins.Text = "-"
+        Button_moins.UseVisualStyleBackColor = False
+        ' 
+        ' Button_div
+        ' 
+        Button_div.BackColor = SystemColors.ActiveCaptionText
+        Button_div.Font = New Font("Showcard Gothic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_div.ForeColor = SystemColors.ButtonFace
+        Button_div.Location = New Point(602, 235)
+        Button_div.Name = "Button_div"
+        Button_div.Size = New Size(147, 50)
+        Button_div.TabIndex = 13
+        Button_div.Text = "/"
+        Button_div.UseVisualStyleBackColor = False
+        ' 
+        ' Button_fois
+        ' 
+        Button_fois.BackColor = SystemColors.ActiveCaptionText
+        Button_fois.Font = New Font("Showcard Gothic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_fois.ForeColor = SystemColors.ButtonFace
+        Button_fois.Location = New Point(423, 235)
+        Button_fois.Name = "Button_fois"
+        Button_fois.Size = New Size(154, 49)
+        Button_fois.TabIndex = 14
+        Button_fois.Text = "*"
+        Button_fois.UseVisualStyleBackColor = False
+        ' 
+        ' Button_egal
+        ' 
+        Button_egal.BackColor = SystemColors.ActiveCaptionText
+        Button_egal.FlatStyle = FlatStyle.Popup
+        Button_egal.Font = New Font("Segoe UI Symbol", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_egal.ForeColor = SystemColors.ButtonHighlight
+        Button_egal.Location = New Point(423, 302)
+        Button_egal.Name = "Button_egal"
+        Button_egal.Size = New Size(326, 56)
+        Button_egal.TabIndex = 16
+        Button_egal.Text = "="
+        Button_egal.UseVisualStyleBackColor = False
+        ' 
+        ' Button_9
+        ' 
+        Button_9.BackColor = SystemColors.ActiveCaption
+        Button_9.FlatStyle = FlatStyle.Popup
+        Button_9.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button_9.Location = New Point(12, 316)
+        Button_9.Name = "Button_9"
+        Button_9.Size = New Size(328, 29)
+        Button_9.TabIndex = 17
+        Button_9.Text = "9"
+        Button_9.UseVisualStyleBackColor = False
+        ' 
+        ' calul_pro
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button_9)
+        Controls.Add(Button_egal)
+        Controls.Add(Button_fois)
+        Controls.Add(Button_div)
+        Controls.Add(Button_moins)
+        Controls.Add(Button_plus)
+        Controls.Add(Button_c)
+        Controls.Add(Button_8)
+        Controls.Add(Button_7)
+        Controls.Add(button_6)
+        Controls.Add(Button_5)
+        Controls.Add(Button_4)
+        Controls.Add(button_3)
+        Controls.Add(Button_2)
+        Controls.Add(Button_1)
+        Controls.Add(Button_0)
+        Controls.Add(TextBox)
+        ForeColor = SystemColors.ActiveCaptionText
+        Name = "calul_pro"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox As TextBox
+    Friend WithEvents Button_0 As Button
+    Friend WithEvents Button_1 As Button
+    Friend WithEvents Button_2 As Button
+    Friend WithEvents button_3 As Button
+    Friend WithEvents Button_4 As Button
+    Friend WithEvents Button_5 As Button
+    Friend WithEvents button_6 As Button
+    Friend WithEvents Button_7 As Button
+    Friend WithEvents Button_8 As Button
+    Friend WithEvents Button_c As Button
+    Friend WithEvents Button_plus As Button
+    Friend WithEvents Button_moins As Button
+    Friend WithEvents Button_div As Button
+    Friend WithEvents Button_fois As Button
+    Friend WithEvents Button_egal As Button
+    Friend WithEvents Button_9 As Button
+
+End Class
---
+++ calul_pro.vb
@@ -0,0 +1,146 @@
+﻿
+Public Class calul_pro
+    Dim a As String
+    Dim b As String
+    Dim op = ""
+    Dim test As Boolean = False
+    Dim res As Object
+    Private Sub Button_0_Click(sender As Object, e As EventArgs) Handles Button_0.Click
+        If op = "" Then
+            A = a + 0
+        Else
+            B = b + 0
+        End If
+    End Sub
+
+    Private Sub Button_1_Click(sender As Object, e As EventArgs) Handles Button_1.Click
+        If op = "" Then
+            A = a + 1
+        Else
+            B = b + 1
+        End If
+    End Sub
+
+    Private Sub Button_egal_Click(sender As Object, e As EventArgs) Handles Button_egal.Click
+        If IsNumeric(a) And IsNumeric(b) Then
+            Dim test = True
+            Dim va = Integer.Parse(a)
+            Dim vb = Integer.Parse(b)
+
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va + vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" Then
+                res = va / vb
+            Else
+                MessageBox.Show("division par 0")
+                test = False
+
+            End If
+            If test Then
+                res.text = res.text + "=" + res.ToString + "" + op + "" + b.ToString + "=" + res.ToString
+            Else
+                MessageBox.Show("a et b doivent etre des entiers ")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button_div_Click(sender As Object, e As EventArgs) Handles Button_div.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        res.text = a + "" + op + "" + b
+    End Sub
+
+    Private Sub Button_plus_Click(sender As Object, e As EventArgs) Handles Button_plus.Click
+        If Not a = "" Then
+            op = "+"
+        End If
+        Res.text = a + "" + op + "" + b
+    End Sub
+
+    Private Sub Button_fois_Click(sender As Object, e As EventArgs) Handles Button_fois.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        res.text = a + "" + op + "" + b
+    End Sub
+    Private Sub Button_moins_Click(sender As Object, e As EventArgs) Handles Button_moins.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        Res.text = a + "" + op + "" + b
+
+    End Sub
+
+    Private Sub Button_c_Click(sender As Object, e As EventArgs) Handles Button_c.Click
+        res.text = ""
+    End Sub
+
+    Private Sub Button_2_Click(sender As Object, e As EventArgs) Handles Button_2.Click
+        If op = "" Then
+            A = a + 2
+        Else
+            B = b + 2
+        End If
+    End Sub
+
+    Private Sub button_3_Click(sender As Object, e As EventArgs) Handles button_3.Click
+        If op = "" Then
+            A = a + 3
+        Else
+            B = b + 3
+        End If
+    End Sub
+
+    Private Sub Button_4_Click(sender As Object, e As EventArgs) Handles Button_4.Click
+        If op = "" Then
+            A = a + 4
+        Else
+            B = b + 4
+        End If
+    End Sub
+
+    Private Sub Button_5_Click(sender As Object, e As EventArgs) Handles Button_5.Click
+        If op = "" Then
+            A = a + 5
+        Else
+            B = b + 5
+        End If
+    End Sub
+
+    Private Sub button_6_Click(sender As Object, e As EventArgs) Handles button_6.Click
+        If op = "" Then
+            A = a + 6
+        Else
+            B = b + 6
+        End If
+    End Sub
+
+    Private Sub Button_7_Click(sender As Object, e As EventArgs) Handles Button_7.Click
+        If op = "" Then
+            A = a + 7
+        Else
+            B = b + 7
+        End If
+    End Sub
+
+    Private Sub Button_8_Click(sender As Object, e As EventArgs) Handles Button_8.Click
+        If op = "" Then
+            A = a + 8
+        Else
+            B = b + 8
+        End If
+    End Sub
+
+    Private Sub Button_9_Click(sender As Object, e As EventArgs) Handles Button_9.Click
+        If op = "" Then
+            A = a + 9
+        Else
+            B = b + 9
+        End If
+    End Sub
+End Class
